9f5f2970bed2b4a20427910d668cd7d7609ffa47
vegeta2102 <hoangvmh@gmail.com>
2020-04-11 12:53:43 +0900

    add firebase ml kit

--- app/src/main/java/jp/hoangvu/barcode_reader/MainActivity.kt
+++ app/src/main/java/jp/hoangvu/barcode_reader/MainActivity.kt
@@ -5,6 +5,8 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,6 +21,12 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+        val options = FirebaseVisionBarcodeDetectorOptions.Builder()
+            .setBarcodeFormats(
+                FirebaseVisionBarcode.FORMAT_QR_CODE,
+                FirebaseVisionBarcode.FORMAT_AZTEC)
+            .build()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
